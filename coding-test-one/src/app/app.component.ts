64da0ca343fed677943e6fd302edd07dc7a0b191
nicoisz <nicolasignacio.sz@gmail.com>
2022-07-31 00:12:02 -0400

    validations of fields, and buttons

--- coding-test-one/src/app/app.component.ts
+++ coding-test-one/src/app/app.component.ts
@@ -3,38 +3,37 @@
 import { Component, OnInit } from '@angular/core';
 import { TitleService } from './service/title.service';
 import { Title } from './model/title.model';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
  
   public titles: Title[]=[];
   form= this.fb.group({
     title:[''],
     firstName:[''],
-    secondName:[''],
+    secondName:['', Validators.required],
     termsAccepted:[''],
   });
-  
+  public errorClass: string | undefined;
   constructor(private titleService: TitleService, private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-
+  ngOnInit() {
     this.titleService.getTitles().subscribe((response) => {
       this.titles = response.filter((r)=> r.name != "!").sort((a ,b)=> a.name.localeCompare(b.name));
-    })
-
-    
-    
+    });
   }
 
   onSubmit():void{
     let result = this.form.value;
-    console.log("result form:", result);
+    if(this.form.status == 'VALID'){
+      console.log("result form:", result);
+    }
+    
   }
 
 }
