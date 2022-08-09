4363eec30f32d9808c4ef94801533cde1ea293e3
nicoisz <nicolasignacio.sz@gmail.com>
2022-08-08 23:07:51 -0400

    todo app

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
+import { TodosComponent } from './components/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
